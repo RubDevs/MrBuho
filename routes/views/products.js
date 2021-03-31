509272ead7337d1bf9d751d79840d24edde041a7
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-03-31 11:44:22 -0600

    build script done

--- routes/views/products.js
+++ routes/views/products.js
@@ -1,16 +1,17 @@
-const express = require("express")
-const router = express.Router()
-const ProductsService = require("../../services/products")
+const { config } = require("../../config");
+const express = require("express");
+const router = express.Router();
+const ProductsService = require("../../services/products");
 
-const productService = new ProductsService()
-router.get("/", async function(req,res,next){
-    const { tags } = req.query
-    try {
-        const products = await productService.getProducts({ tags })
-        res.render("products", { products })
-    } catch (error) {
-        next(error)
-    }
-})
+const productService = new ProductsService();
+router.get("/", async function (req, res, next) {
+  const { tags } = req.query;
+  try {
+    const products = await productService.getProducts({ tags });
+    res.render("products", { products, dev: config.dev });
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
