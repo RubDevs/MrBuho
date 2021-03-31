66db5de61bd19a82127a5140719207454584abf5
Rubén Hernández <69823633+RubDevs@users.noreply.github.com>
2021-03-31 12:19:43 -0600

    cache control (#7)

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,109 +1,120 @@
-const express = require("express")
-const passport = require("passport")
-const ProductsService = require("../../services/products")
-const validation = require('../../utils/middleware/validationHandler')
-const { 
-    createProductSchema, 
-    updateProductSchema, 
-    productIdSchema, 
-    productTagSchema 
-} = require('../../utils/schema/products')
+const express = require("express");
+const passport = require("passport");
+const ProductsService = require("../../services/products");
+const validation = require("../../utils/middleware/validationHandler");
+const {
+  createProductSchema,
+  updateProductSchema,
+  productIdSchema,
+  productTagSchema,
+} = require("../../utils/schema/products");
+const cacheResponse = require("../../utils/cacheResponse");
+const {
+  FIVE_MINUTES_IN_SECONDS,
+  SIXTY_MINUTES_IN_SECONDS,
+} = require("../../utils/time");
 
 //jwt strategy
-require('../../utils/auth/strategies/jwt')
+require("../../utils/auth/strategies/jwt");
 
 function productsApi(app) {
-    const router = express.Router()
-    app.use("/api/products",router)
-    const productService = new ProductsService()
+  const router = express.Router();
+  app.use("/api/products", router);
+  const productService = new ProductsService();
 
-    router.get("/",async function(req,res,next){
-        const { tags } = req.query
+  router.get("/", async function (req, res, next) {
+    cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
+    const { tags } = req.query;
 
-        try {
-            const products = await productService.getProducts({ tags })
+    try {
+      const products = await productService.getProducts({ tags });
 
-            res.status(200).json({
-                data: products,
-                message: "products listed"
-            }) 
-        } catch (error) {
-            next(error)
-        }
-        
-    })
+      res.status(200).json({
+        data: products,
+        message: "products listed",
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 
-    router.get("/:productId",async function(req,res,next){
-        const { productId } = req.params
+  router.get("/:productId", async function (req, res, next) {
+    cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
+    const { productId } = req.params;
 
-        try {
-            const product = await productService.getProduct({ productId}) 
+    try {
+      const product = await productService.getProduct({ productId });
 
-            res.status(200).json({
-                data: product,
-                message: "product retrieved"
-            }) 
-        } catch (error) {
-            next(error)
-        }
-        
-    })
+      res.status(200).json({
+        data: product,
+        message: "product retrieved",
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
 
-    router.post("/",validation(createProductSchema),async function(req,res,next){
-        const { body: product } = req
+  router.post(
+    "/",
+    validation(createProductSchema),
+    async function (req, res, next) {
+      const { body: product } = req;
 
-        try {
-            const createdproduct = await productService.createProduct({ product })
+      try {
+        const createdproduct = await productService.createProduct({ product });
 
-            res.status(201).json({
-            data: createdproduct,
-            message: "product created"
-        })
-        } catch (error) {
-            next(error)
-        }
-        
-    })
+        res.status(201).json({
+          data: createdproduct,
+          message: "product created",
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
 
-    router.put(
-        "/:productId",
-        passport.authenticate("jwt",{ session: false }),
-        validation({"productId": productIdSchema}, "params"),
-        validation(updateProductSchema),
-        async function(req,res,next){
-        const { productId } = req.params
-        const { body: product } = req
-        
-        try {
-            const updatedproduct = await productService.updateProduct({ productId, product })
-            res.status(200).json({
-                data: updatedproduct,
-                message: "product updated"
-            }) 
-        } catch (error) {
-            next(error)   
-        }
-        
-    })
+  router.put(
+    "/:productId",
+    passport.authenticate("jwt", { session: false }),
+    validation({ productId: productIdSchema }, "params"),
+    validation(updateProductSchema),
+    async function (req, res, next) {
+      const { productId } = req.params;
+      const { body: product } = req;
 
-    router.delete("/:productId",
-        passport.authenticate("jwt",{ session: false }),
-        validation(productIdSchema),
-        async function(req,res,next)
-    {
-        const { productId } = req.params
+      try {
+        const updatedproduct = await productService.updateProduct({
+          productId,
+          product,
+        });
+        res.status(200).json({
+          data: updatedproduct,
+          message: "product updated",
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
 
-        try {
-            const product = await productService.deleteProduct({ productId })
-            res.status(200).json({
-            data: product,
-            message: "product deleted"
-        })
-        } catch (error) {
-            next(error)
-        }
-        
-    })
+  router.delete(
+    "/:productId",
+    passport.authenticate("jwt", { session: false }),
+    validation(productIdSchema),
+    async function (req, res, next) {
+      const { productId } = req.params;
+
+      try {
+        const product = await productService.deleteProduct({ productId });
+        res.status(200).json({
+          data: product,
+          message: "product deleted",
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
 }
 
-module.exports = productsApi
+module.exports = productsApi;
--- routes/views/products.js
+++ routes/views/products.js
@@ -2,9 +2,12 @@ const { config } = require("../../config");
 const express = require("express");
 const router = express.Router();
 const ProductsService = require("../../services/products");
+const cacheResponse = require("../../utils/cacheResponse");
+const { FIVE_MINUTES_IN_SECONDS } = require("../../utils/time");
 
 const productService = new ProductsService();
 router.get("/", async function (req, res, next) {
+  cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
   const { tags } = req.query;
   try {
     const products = await productService.getProducts({ tags });
